70768588fae57e486ebb0d8887565014055aa432
Макс третьяков <saiktm102@yandex.ru>
2023-08-03 22:00:22 +0700

    1

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,25 @@
 package ru.practicum.shareit.booking.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import ru.practicum.shareit.booking.model.BookingStatus;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
 /**
  * TODO Sprint add-bookings.
  */
+@Getter
+@Setter
+@AllArgsConstructor
 public class BookingDto {
+    private long id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Item item;
+    private User booker;
+    private BookingStatus status;
 }
