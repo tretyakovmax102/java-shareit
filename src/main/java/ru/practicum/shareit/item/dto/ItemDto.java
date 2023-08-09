88e79515f25b717d8509d4c6d30841c2934b9681
tretyakovmax102 <saiktm102@yandex.ru>
2023-08-09 13:30:07 +0700

    Revert "commit 1.0"
    This reverts commit 1e283209

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -4,5 +4,4 @@ package ru.practicum.shareit.item.dto;
  * TODO Sprint add-controllers.
  */
 public class ItemDto {
-    
 }
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,33 +1,7 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import ru.practicum.shareit.user.model.User;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 /**
  * TODO Sprint add-controllers.
  */
-@Builder
-@Getter
-@Setter
-@AllArgsConstructor
 public class Item {
-    private long id;
-    @NotBlank
-    @Size(max = 50)
-    private String name;
-    @NotBlank
-    @Size(max = 200)
-    private String description;
-    @NotNull
-    private Boolean available;
-    @NotNull
-    private User owner;
-    private Long request;
 }
