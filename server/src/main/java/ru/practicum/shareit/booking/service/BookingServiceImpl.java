70a17da39e41f4d2d51a206449b0c3da30eaa64e
tretyakovmax102 <saiktm102@yandex.ru>
2023-09-09 00:07:22 +0700

    sprint 16 fix

--- server/src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -16,10 +16,8 @@ import ru.practicum.shareit.booking.repository.BookingRepository;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.exception.ValidationException;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.item.service.ItemService;
 import ru.practicum.shareit.user.model.User;
-import ru.practicum.shareit.user.repository.UserRepository;
 import ru.practicum.shareit.user.service.UserService;
 
 import java.time.LocalDateTime;
