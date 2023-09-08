94b8bfc00df693ddebb3410d82d3a480014788fb
tretyakovmax102 <saiktm102@yandex.ru>
2023-09-08 21:49:13 +0700

    sprint 16.1

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.booking.dto;
 
 import lombok.*;
 import ru.practicum.shareit.valid.ValidateDate;
-
 import javax.validation.constraints.FutureOrPresent;
 import java.time.LocalDateTime;
 
